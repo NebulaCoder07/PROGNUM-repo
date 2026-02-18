4c97114b005d16aafcc2a0da81cb4be316818a00
B. Balas <bbalas@hydra26.desktop.astro.rug.nl>
2026-02-18 17:21:56 +0100

    week 2 update

---
+++ Task1/myfirst.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# **Question 1.15**
+
+# 1. After opening Downloads in Terminal and executing the pwd command it printed:
+# **/Users/users/bbalas/Downloads**
+
+# 2. **'.'** refers to the **chosen** directory and **'..'** refers to the **parent** directory (one  level higher)
+
+# **Question 1.16**
+
+# If Desktop is a subdirctory, the shortest way is: **cd Desktop**
+
+# In[ ]:
+
+
+
+
